dd1d34a2d02d8b965ac11344c9db81ce9f8df4cd
annatakman <annatakman@gmail.com>
2020-03-30 19:10:10 +0200

    code with problems

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { CocktailList } from 'pages/CocktailList'
+import { CocktailDetails } from 'pages/CocktailDetails'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          < CocktailList />
+        </Route>
+        <Route>
+          <CocktailDetails path="/:cocktailId" exact />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/CocktailDetails.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+export const CocktailDetails = () => {
+  const { cocktailId } = useParams()
+  const [cocktails, setCocktails] = useState([])
+
+  useEffect(() => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
+      .then(res => res.json())
+      .then(json => {
+        setCocktails(json.drinks)
+        console.log(cocktailId)
+      })
+  }, [cocktailId])
+
+  return (
+    <div>
+      {cocktails.map((cocktail) => (
+        <div key={cocktail.idDrink}>
+          <h2>{cocktail.strDrink}</h2>
+          <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+          <h3>Instructions</h3>
+          <p>{cocktail.strInstructions}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
